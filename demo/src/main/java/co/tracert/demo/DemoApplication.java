bea1aa37353d9d5ff2dd6aeeb675cdb7a65d4eea
srikanth.kotekar <srikanth.kotekar@gmail.com>
2018-04-18 23:07:02 +0200

    1

---
+++ demo/src/main/java/co/tracert/demo/DemoApplication.java
@@ -0,0 +1,12 @@
+package co.tracert.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
