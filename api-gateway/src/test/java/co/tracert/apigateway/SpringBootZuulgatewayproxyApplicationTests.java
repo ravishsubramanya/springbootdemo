60e3ecb3a0845a3bd9c6af8cde77297fa04f2c61
srikanthkotekar <srikanth.kotekar@gmail.com>
2018-06-17 22:27:08 +0200

    added tests with rest-assured

--- api-gateway/src/test/java/co/tracert/apigateway/SpringBootZuulgatewayproxyApplicationTests.java
+++ api-gateway/src/test/java/co/tracert/apigateway/SpringBootZuulgatewayproxyApplicationTests.java
@@ -1,5 +1,8 @@
 package co.tracert.apigateway;
 
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.contains;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,7 +13,14 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class SpringBootZuulgatewayproxyApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void givenRequestForProfile_thenResponseCodeIs200() {
+		given().when().get("http://localhost:8080/api/tracert/profile/all").then().statusCode(200);
+	}
+
+	@Test
+	public void givenRequestForProfile_thenResponseContainsSrikanth() {
+		given().when().get("http://localhost:8080/api/tracert/profile/all").then()
+				.body("firstName", contains("Srikanth")).body("lastName", contains("Kotekar")).statusCode(200);
 	}
 
 }
