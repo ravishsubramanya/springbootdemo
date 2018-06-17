d6ae453287eaed0f6bcbeb621a0019b1f152f400
srikanthkotekar <srikanth.kotekar@gmail.com>
2018-06-17 21:32:03 +0200

    initial

---
+++ service-registry/src/main/java/co/tracert/service_registry/ServiceRegistryApp.java
@@ -0,0 +1,13 @@
+package co.tracert.service_registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceRegistryApp {
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryApp.class, args);
+	}
+}
