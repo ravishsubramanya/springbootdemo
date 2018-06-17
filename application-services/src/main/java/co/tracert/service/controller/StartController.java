e7c9b7e537a6ef812f67187d1a527692a070f061
srikanthkotekar <srikanth.kotekar@gmail.com>
2018-06-17 21:27:54 +0200

    ok

--- application-services/src/main/java/co/tracert/service/controller/StartController.java
+++ application-services/src/main/java/co/tracert/service/controller/StartController.java
@@ -3,31 +3,17 @@ package co.tracert.service.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.Status;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import co.tracert.service.data.repositories.ProfileInfoRepository;
 import co.tracert.service.model.Profile;
 
 @RestController
 public class StartController {
 
-	@Autowired
-	ProfileInfoRepository repository;
-
-	@GetMapping("/profile/{profileId}")
-	public Profile getProfile() {
-		List<Profile> result = (List<Profile>) repository.findAll();
-		return (result != null && result.size() > 0) ? result.get(0) : null;
-	}
-
 	@GetMapping("/profile/all")
 	public List<Profile> getAllProfiles() {
-		/*
-		 * List<Profile> result = (List<Profile>) repository.findAll(); return (result
-		 * != null && result.size() > 0) ? result : null;
-		 */
 		List<Profile> profiles = new ArrayList<Profile>();
 		profiles.add(getDummyData());
 		return profiles;
@@ -37,4 +23,10 @@ public class StartController {
 	public Profile getDummyData() {
 		return Profile.getDefault();
 	}
+
+	@GetMapping("/health")
+	public Status health() {
+		return Status.UP;
+	}
+
 }
